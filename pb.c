a2d1c125599d04342ea1565d9fdce9ead67297f9
Tatiana <tatyperson22@gmail.com>
2013-05-08 12:23:19 +0200

    Añadiendo argumento -favorite

--- pb.c
+++ pb.c
@@ -24,42 +24,56 @@ int calculate_result(int white_balls[5], int power_ball)
 
 int main(int argc, char** argv)
 {
-    if (argc != 7)
-    {
-        fprintf(stderr, "Usage: %s power_ball (5 white balls)\n", argv[0]);
-        return -1;
-    }
-
-    int power_ball = atoi(argv[1]);
-
-    int white_balls[5];
-    for (int i=0; i<5; i++)
-    {
-      char* endptr = NULL;
-      long val = strtol(argv[1+i], &endptr, 10);
-      if (*endptr)
+	int balls[6];
+	int count_balls = 0;
+	int favorite = 0;
+	for (int i=1; i<argc; i++)
 	{
-	  fprintf(stderr, "Invalid arguments\n");
-	  return -1;
+		const char* arg = argv[i];
+		if ('-' == arg[0])
+		{
+			if (0 == strcmp(arg, "-favorite"))
+			{
+				favorite = 1;
+			}
+			else
+			{
+				goto usage_error;
+			}
+		}
+		else
+		{
+			char* endptr = NULL;
+			long val = strtol(arg, &endptr, 10);
+			if (*endptr)
+			{
+                           goto usage_error;
+                        }
+                        balls[count_balls++] = (int) val;
+                }
+          }
+        if (6 != count_balls)
+        {
+		goto usage_error;
 	}
-      white_balls[i] = (int) val;
-    }
-      
-
-    int result = calculate_result(white_balls, power_ball);
-
-    if (result < 0)
-      {
-	fprintf(stderr, "Invalid arguments\n");
+	int power_ball = balls[5];
+	int result = calculate_result(balls, power_ball);
+	if (result < 0)
+	{
+		goto usage_error;
+	}
+	if (LUCKY_NUMBER == power_ball)
+	{
+		result = result * 2;
+	}	
+	if (favorite)
+	{
+		result = result * 2;
+	}
+	printf("%d percent chance of winning\n", result);
+	return 0;
+	usage_error:
+	fprintf(stderr, "Usage: %s [-favorite] (5 white balls)
+	power_ball\n", argv[0]);
 	return -1;
-      }
-
-    if (LUCKY_NUMBER == power_ball)
-      {
-	result = result * 2; //Probabilidad doble
-      }
-
-    printf("%d percent chance of winning\n", result);
-
-    return 0;
-}
+	}
