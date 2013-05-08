c5fce1b564680a4ff9245c5353c929d964c2f1b1
Tatiana <tatyperson22@gmail.com>
2013-05-08 13:30:31 +0200

    Comentarios

--- pb.c
+++ pb.c
@@ -34,10 +34,7 @@ int calculate_result(int white_balls[5], int power_ball)
 	  return -1;
 	}
     }
-  if ((power_ball < 1) || (power_ball > MAX_POWER_BALL))
-    {
-      return -1;
-    }
+// lottery ball numbers are always shown sorted
   qsort(white_balls, 5, sizeof(int), my_sort_func);
   return 0;
 }
@@ -46,53 +43,36 @@ int main(int argc, char** argv)
 {
   int balls[6];
   int count_balls = 0;
-  int favorite = 0;
+  int favorite = 0; //this should be a bool
   for (int i=1; i<argc; i++)
-    {
-      const char* arg = argv[i];
-      if ('-' == arg[0])
-	{
-	  if (0 == strcmp(arg, "-favorite"))
-	    {
-	      favorite = 1;
-	    }
-	  else
-	    {
-	      goto usage_error;
-	    }
-	}
-      else
-	{
-	  char* endptr = NULL;
-	  long val = strtol(arg, &endptr, 10);
-	  if (*endptr)
-	    {
-	      goto usage_error;
-	    }
-	  balls[count_balls++] = (int) val;
-	}
-    }
-  if (6 != count_balls)
-    {
-      goto usage_error;
-    }
+  {
+  	goto usage_error;
+	 
+   }
+ // the power ball is always the last one given
   int power_ball = balls[5];
+  
   int result = calculate_result(balls, power_ball);
+
+// calculate result can return -1 if the ball numbers
+// are out of range
+
   if (result < 0)
     {
       goto usage_error;
-    }
-  if (LUCKY_NUMBER == power_ball)
-    {
-      result = result * 2;
+    
+  	if (LUCKY_NUMBER == power_ball)
+    	{
+      	result = result * 2;
+   	 }	
+  	if (favorite)
+    	{
+      	result = result * 2;
+    	}
+  	printf("%d percent chance of winning\n", result);
+  	return 0;
+ 	usage_error:
+  	fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power_ball\n", argv[0]);
     }	
-  if (favorite)
-    {
-      result = result * 2;
-    }
-  printf("%d percent chance of winning\n", result);
-  return 0;
- usage_error:
-  fprintf(stderr, "Usage: %s [-favorite] (5 white balls) power_ball\n", argv[0]);
   return -1;
 }
