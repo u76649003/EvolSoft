a0c1a2578cf8928b10592f35052028c9f9fd6817
Alecia Reid <al.reid@alum.uca.es>
2013-05-09 16:50:56 +0200

    Función My_sort_func

--- pb.c
+++ pb.c
@@ -9,24 +9,20 @@
 static int my_sort_func(const void* p1, const void* p2) {
      int v1 = *((int *) p1);
      int v2 = *((int *) p2);
-if (v1 < v2)
- 
-{
-return -1;
-    }
-    else if (v1 > v2)
+   
+   if (v1 < v2)
     {
-return 1; }
-else {
-return 0; }
+        return -1;
+   }
+   else if (v1 > v2)
+   {
+       return 1;
+   }
+   else
+     {
+        return 0;
+     }
 }
-int calculate_result(int white_balls[5], int power_ball) { 
-for (int i=0; i<5; i++)
-    {
-...
-return -1; }
-qsort(white_balls, 5, sizeof(int), my_sort_func);
-return 0; }
 
 
 int calculate_result(int white_balls[5], int power_ball)
@@ -42,6 +38,7 @@ int calculate_result(int white_balls[5], int power_ball)
     {
       return -1;
     }
+  qsort(white_balls, 5, sizeof(int), my_sort_func);
   return 0;
 }
 
